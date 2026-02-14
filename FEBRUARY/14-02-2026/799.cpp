ccaa2ade93bfdd364c99e2db5bbb31cfa82f7ffe
rajectedguy <rajectedguy@gmail.com>
2026-02-14 22:42:00 +0530

    14-02-2026

---
+++ FEBRUARY/14-02-2026/799.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    double champagneTower(int poured, int query_row, int query_glass) {
+        std::vector<double> prev(1, (double)poured);
+
+        for (int i = 1; i <= query_row; i++) {
+            std::vector<double> curr(i + 1, 0.0);
+            for (int j = 0; j < i; j++) {
+                double extra = prev[j] - 1.0;
+                if (extra > 0.0) {
+                    curr[j] += extra * 0.5;
+                    curr[j + 1] += extra * 0.5;
+                }
+            }
+            prev.swap(curr);
+        }
+
+        return std::min(1.0, prev[query_glass]);
+    }
+};
