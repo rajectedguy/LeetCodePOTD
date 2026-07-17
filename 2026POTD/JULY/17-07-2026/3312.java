6ca0c12aa7a8c80f7e72134ffe8c71d9d9a25849
rajectedguy <rajectedguy@gmail.com>
2026-07-17 22:18:28 +0530

    17-07-2026

---
+++ 2026POTD/JULY/17-07-2026/3312.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int[] gcdValues(int[] nums, long[] queries) {
+        int maxVal = 0;
+        for (int x : nums) maxVal = Math.max(maxVal, x);
+        long[] freq = new long[maxVal + 1];
+        for (int x : nums) freq[x]++;
+        long[] divCnt = new long[maxVal + 1];
+        for (int g = 1; g <= maxVal; g++) {
+            for (int x = g; x <= maxVal; x += g) {
+                divCnt[g] += freq[x];
+            }
+        }
+        long[] exact = new long[maxVal + 1];
+        for (int g = maxVal; g >= 1; g--) {
+            exact[g] = divCnt[g] * (divCnt[g] - 1) / 2;
+            for (int m = 2 * g; m <= maxVal; m += g) {
+                exact[g] -= exact[m];
+            }
+        }
+        long[] prefix = new long[maxVal + 1];
+
+        for (int g = 1; g <= maxVal; g++) {
+            prefix[g] = prefix[g - 1] + exact[g];
+        }
+        int[] ans = new int[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            long target = queries[i] + 1;
+            int left = 1, right = maxVal;
+            while (left < right) {
+                int mid = left + (right - left) / 2;
+                if (prefix[mid] >= target)
+                    right = mid;
+                else
+                    left = mid + 1;
+            }
+            ans[i] = left;
+        }
+        return ans;
+    }
+}
