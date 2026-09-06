2a5c578d21638e4e9725c755c54a6acbfb0adce1
rajectedguy <rajectedguy@gmail.com>
2026-09-06 22:23:03 +0530

    06-09-2026

--- 2026POTD/SEPTEMBER/06-09-2026/115.java
+++ 2026POTD/SEPTEMBER/06-09-2026/115.java
@@ -1,20 +1,20 @@
-class Solution {
-    public int numDistinct(String s, String t) {
+class Solution{
+    public int numDistinct(String s, String t){
         int m = s.length(), n = t.length();
-        if (m < n) {
+        if (m < n){
             return 0;
         }
         int[][] dp = new int[m + 1][n + 1];
-        for (int i = 0; i <= m; i++) {
+        for (int i = 0; i <= m; i++){
             dp[i][n] = 1;
         }
-        for (int i = m - 1; i >= 0; i--) {
+        for (int i = m - 1; i >= 0; i--){
             char sChar = s.charAt(i);
-            for (int j = n - 1; j >= 0; j--) {
+            for (int j = n - 1; j >= 0; j--){
                 char tChar = t.charAt(j);
-                if (sChar == tChar) {
+                if (sChar == tChar){
                     dp[i][j] = dp[i + 1][j + 1] + dp[i + 1][j];
-                } else {
+                }else{
                     dp[i][j] = dp[i + 1][j];
                 }
             }
